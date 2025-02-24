2cf836b8b02c096790d7f0b2b3045fb531f3e1d0
Wutt Saeng <wuttforwork@gmail.com>
2025-02-24 17:08:12 +0700

    ADD:primitive-values Exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let DiscountPercentage = (25 / 100)*totalPrice ;
+let finalPrice = totalPrice - DiscountPercentage
+console.log(finalPrice);
+
+
